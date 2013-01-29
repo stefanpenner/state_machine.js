2a6655044c1581fb99660831ff5a462a8026577f
Stefan Penner <stefan.penner@gmail.com>
2013-01-29 16:38:40 -0500

    Object.create polyfil

--- state_machine.js
+++ state_machine.js
@@ -31,6 +31,16 @@ if (!Array.prototype.indexOf) {
   }
 }
 
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
+}
 var slice = Array.prototype.slice;
 
 function StateMachine(options){
